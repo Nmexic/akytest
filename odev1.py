abe9b97cda7de4d592c33cb4ef4b061ad3e189d8
Nmexic <mbal7759@gmail.com>
2023-12-13 19:14:59 +0300

    odev1

--- odev1.py
+++ odev1.py
@@ -1,5 +1,5 @@
 import requests
-url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400
+url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
 response = requests.get(url)
 data = response.json()
 print(data)
